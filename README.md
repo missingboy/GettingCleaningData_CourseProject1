ad6e872d17b059db8cc836db2e748ff8ff3af109
missingboy <Li.Jiang@umassmed.edu>
2014-05-25 01:33:30 -0400

    Submission of R code of course project

--- README.md
+++ README.md
@@ -2,3 +2,31 @@ GettingCleaningData_CourseProject1
 ==================================
 
 The repository for courese project of Getting and Cleaning Data from Coursera 
+
+## 1. Merge the training and the test sets to create one data set
+      xTrain.txt, yTrain.txt and subTrain are combined to form the training set by cbind.
+      xTest.txt, yTest.txt and subTrain are combined to form the test set by cbind.
+      features are read into the R environment and "subject" (numbering of each pariticipated subject) and             "activity" (code of each type of activity) are appended into features as complete labels of training set          and test set.
+      Training set and test set are combined by rbind and named by features.  
+
+
+## 2. Subset complete dataset to get columns with mean or std
+      All columns with mean and standard deviation of measurements are selected and isolated to form a subset.
+
+## 3. Convert numbering of activities into descriptive terms 
+      For the subset from 2, convert the coding of activities by numbers into descriptive terms, like walking,       standing, etc.
+
+## 4. Re-label the dataset with descriptive activity names  
+      Clean the labels of each variable by: 
+      (1) Expand each abbreviation into full terms.
+      (2) Remove labels like underscores.
+      (3) Remove ambiguious or repeated terms like "fBody".
+      (4) Remove number at the beginning of each variable name. 
+      Output the cleaned file into a text file named "SamsungActivity.txt".
+
+## 5. Creates a second, independent tidy data set with the average of each variable for each activity and each ##    subject
+      Reshape the data from 4 by using "subject" and "acitivity" as id variable and the rest as values.
+      Recast the reshaped data by calculating the mean of each variable except "subject" and "activity". 
+      Output the cleaned file into a text file named "SamsungActivityMean.txt".
+
+        
---
+++ run_analysis.R
@@ -0,0 +1,75 @@
+## Load "reshape2" package
+library(reshape2)
+
+## Download, unzip and load data 
+setwd("D:/R/R-3.0.3/wd/")
+fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+if (!file.exists("./data")){dir.create("./data")}
+download.file(fileUrl,destfile="./data/smartphone.zip", method= "internal")
+unzip("./data/smartphone.zip",exdir="./data")
+
+## Generate training set
+
+xTrain <- read.table("./data/UCI HAR Dataset/train/X_train.txt")
+yTrain <- read.table("./data/UCI HAR Dataset/train/y_train.txt")
+subTrain <- read.table("./data/UCI HAR Dataset/train/subject_train.txt")
+trainSet <- cbind(subTrain,yTrain,xTrain)
+xLabel <- readLines("./data/UCI HAR Dataset/features.txt")
+xLabel <- append(xLabel,"subject",after=0)
+xLabel <- append(xLabel,"activity",after=1)
+colnames(trainSet) <- xLabel
+
+## Generate testset 
+
+xTest <- read.table("./data/UCI HAR Dataset/test/X_test.txt")
+yTest <- read.table("./data/UCI HAR Dataset/test/y_test.txt")
+subTest <- read.table("./data/UCI HAR Dataset/test/subject_test.txt")
+testSet <- cbind(subTest,yTest,xTest)
+colnames(testSet) <- xLabel
+
+## Combine trainset and testset 
+combined1 <- rbind(xTrain, xTest)
+combined2 <- rbind(yTrain, yTest)
+combined3 <- rbind(subTrain,subTest)
+completeSet <- cbind(combined3,combined2,combined1)
+colnames(completeSet) <- xLabel 
+
+## Subset complete dataset to get columns with mean or std, 
+## together with subject number and activity 
+meanCol <- grep("mean",xLabel,ignore.case=TRUE)
+sdCol <- grep("std",xLabel)
+subset <- c(c(1,2),meanCol, sdCol)
+subset <- sort(subset)
+meanSD <- completeSet[ ,subset]
+
+## Convert numbering of activities into descriptive terms 
+meanSD[ ,2] <- gsub(1,"walking",meanSD[ ,2])
+meanSD[ ,2] <- gsub(2,"walkingUpstairs",meanSD[ ,2])
+meanSD[ ,2] <- gsub(3,"walkingDownstairs",meanSD[ ,2])
+meanSD[ ,2] <- gsub(4,"sitting",meanSD[ ,2])
+meanSD[ ,2] <- gsub(5,"standing",meanSD[ ,2])
+meanSD[ ,2] <- gsub(6,"laying",meanSD[ ,2])
+meanSD[ ,2] <- as.factor(meanSD[ ,2])
+meanSD[ ,1] <- as.factor(meanSD[ ,1])
+
+## Re-label the dataset with descriptive activity names 
+columnNames <- names(meanSD)
+columnNames <- gsub("Acc","Acceleration",columnNames)
+columnNames <- gsub("Gyro","Gyroscope",columnNames)
+columnNames <- gsub("-","",columnNames)
+columnNames <- gsub("\\()","",columnNames)
+columnNames <- gsub("mean","Mean",columnNames)
+columnNames <- gsub("std","Std",columnNames)
+columnNames <- gsub("fBody","",columnNames)
+columnNames <- gsub("tBody","",columnNames)
+columnNames <- gsub("^[0-9] |^[0-9][0-9] |^[0-9][0-9][0-9] ","",columnNames)
+colnames(meanSD) <- columnNames
+write.table(meanSD, file="SamsungActivity.txt", row.names = FALSE, 
+            col.names=TRUE,sep="\t")
+
+## Creates a second, independent tidy data set with the average of each 
+## variable for each activity and each subject.
+meanSDl <- melt(meanSD,id=c("subject","activity"))
+meanSDw <- dcast (meanSDl, subject+activity~variable, mean)
+write.table(meanSD, file="SamsungActivityMean.txt", row.names = FALSE, 
+            col.names=TRUE,sep="\t")
